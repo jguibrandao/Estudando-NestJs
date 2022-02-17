65f4f79c4c0274fcdf5a6ba2e1aa3ea741890ef4
jguibrandao <jguibrandao@gmail.com>
2022-02-17 14:27:48 -0300

    Adicionando classe Course (um modelo basicamente) e terminando de estudar os métodos do course.service

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,6 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { Course } from './entities/course.entity';
 
 @Injectable()
 export class CoursesService {
-    private courses = []
+    private courses: Course[] = [
+        {
+        id: 1,
+        name: "NestJS",
+        description: "Fundamentos do NestJS",
+        tags: ["javascript", "typescript", "express", "nodeJS", "nestjs"]
+    }
+    ]
+
+    findAll() {
+        return this.courses
+    }
+
+    findOne(id: string) {
+        return this.courses.find((course: Course) => course.id === Number(id))
+    }
+
+    create(createCourseDto: any) {
+        this.courses.push(createCourseDto)
+    }
+
+    update(id: string, updateCourseDto: any) {
+        const indexCourse = this.courses.findIndex(
+            (course: Course) => course.id === Number(id)
+        )
+
+        this.courses[indexCourse] = updateCourseDto
+    }
+
+    remove(id: string) {
+        const indexCourse = this.courses.findIndex(
+            (course: Course) => course.id === Number(id)
+        )
+        
+        if(indexCourse >= 0) {
+            this.courses.splice(indexCourse)
+        }
+        
+    }
 }
